106467833c8977e69e9eb2987649d8ba2f7d6ff2
jitprosen360 <jitprosen360@gmail.com>
2023-09-21 21:14:45 +0530

    style changes one

--- config/session-cors.js
+++ config/session-cors.js
@@ -4,7 +4,7 @@ module.exports = (strapi) => {
         strapi.app.use(async (ctx, next) => {
           if (ctx.path === '/api/auth/session') {
             // Customize CORS headers for the /api/auth/session path
-            ctx.set('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+          //  ctx.set('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
             ctx.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
             ctx.set('Access-Control-Allow-Headers', 'Content-Type');
           }
